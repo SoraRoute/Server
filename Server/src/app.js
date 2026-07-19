65e1c110a2ef2eb4d0c4b922f1cc1b08a051a9d2
nishtha322 <nishthasrivastava1602@gmail.com>
2026-07-19 22:44:42 +0530

    change app.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,13 +1,5 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-app.use(express.json());
 const cookieParser = require("cookie-parser");
 
 app.use(cookieParser());
