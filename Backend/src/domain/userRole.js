9a2fc1345d92b6d38dc7195f075fddc5d852aec0
pinkikumari-33 <19kumari.pinki22@gmail.com>
2026-07-24 14:14:25 +0530

    Remove unwanted folder from main branch

--- Backend/src/domain/userRole.js
+++
@@ -1,7 +0,0 @@
-const UserRole=Object.freeze({
-
-    ADMIN:'ROLE_ADMIN',
-    CUSTOMER:'ROLE_CUSTOMER',
-    SELLER:'ROLE_SELLER'
-})
-module.exports=UserRole
--- Backend/src/routers/AuthRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const authController = require("../controller/authController");
-const router=express.Router();
-
-router.post("/sent/login-signup-otp",authController.sendLoginOtp)
-module.exports=router;
